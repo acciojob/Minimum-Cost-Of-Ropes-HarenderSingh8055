4531980f40e5aa9fc6989b942137855421cbe7a2
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-03-31 00:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-function minCostToFormRope(event) {
-	event.preventDefault();
-	var inputElement = document.querySelector('input').value;
-	var arr = inputElement.split(',');
-	 arr.sort(function (a,b){return a-b});
+function minCostToFormRope() {
+	// event.preventDefault();
+	var firstinput=document.querySelector("input").value;
+    var arr=firstinput.split(",");
+    arr.sort(function (a, b) {
+    return a - b;
+  });
 
-	var cost = 0;
-	while (arr.length >1) {
-		var res = Number(arr[0]) + Number(arr[1]);
-		arr.push(res);
-		cost +=res;
-		// Use Shift or splice
-		arr.shift();
-		arr.shift();
-		// arr.splice(0,2);
-		
-		arr.sort(function (a,b){return a-b});
-	}
-	document.getElementById("result").textContent = cost;
+var cost=0;
+while(arr.length>1){
+	var result=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(result);
+	cost+=result;
+	arr.sort(function(a,b){
+		return a-b;
+	});
+}
+document.getElementById("result").textContent=cost;	
+}
+/// {4,2,7,6,9}
+// function minCostToFormRope() {
+// 	// event.preventDefault();
+// 	var inputData = document.querySelector("input").value;
+// 	var arr = inputData.split(",");
+// 	arr.sort(function(a,b){return a-b});
+
+// 	var cost = 0;
+// 	while(arr.length>1){
+// 		var result = Number(arr[0]) + Number(arr[1]); //6
+// 		arr.splice(0,2); //7,6,9
+// 		arr.push(result);
+// 		cost = cost + result;
+// 		arr.sort(function(a,b){ return a-b});
+// 	}
+// 	document.getElementById("result").textContent=result;	
+// }
