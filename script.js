44d599f57fd561ab4755c46ba07fe8ce8c1036bc
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-03-31 01:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
-function calculateMinCost() {
-const ropeLengthsStr = document.getElementById('rope-lengths').value;
-const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
-const n = ropeLengthsArr.length;
+function minCostToFormRope() {
+	// event.preventDefault();
+	var firstinput=document.querySelector("#rope-lengths").value;
+    var arr=firstinput.split(",");
+    arr.sort(function (a, b) {
+    return a - b;
+  });
 
-let pq = [];
-for (let i = 0; i < n; i++) {
-pq.push(ropeLengthsArr[i]);
+var cost=0;
+while(arr.length>1){
+	var result=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(result);
+	cost+=result;
+	arr.sort(function(a,b){
+		return a-b;
+	});
 }
-pq.sort(function (a, b) { return a - b; });
-
-let res = 0;
-while (pq.length > 1) {
-let first = pq.shift();
-let second = pq.shift();
-res += first + second;
-pq.push(first + second);
-pq.sort(function (a, b) { return a - b; });
+document.getElementById("result").textContent=cost;	
 }
-
-document.getElementById('result').textContent = res;
-}  
-
-
-// function minCostToFormRope() {
-// 	// event.preventDefault();
-// 	var firstinput=document.querySelector("#rope-lengths").value;
-//     var arr=firstinput.split(",");
-//     arr.sort(function (a, b) {
-//     return a - b;
-//   });
-
-// var cost=0;
-// while(arr.length>1){
-// 	var result=Number(arr[0])+Number(arr[1]);
-// 	arr.splice(0,2);
-// 	arr.push(result);
-// 	cost+=result;
-// 	arr.sort(function(a,b){
-// 		return a-b;
-// 	});
-// }
-// document.getElementById("result").textContent=cost;	
-// }
 /// {4,2,7,6,9}
 // function minCostToFormRope() {
 // 	// event.preventDefault();
